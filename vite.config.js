3c5580ebcfa664929167a11f75f0460737caceb8
Mike Novikov <NovikovM18>
2024-10-14 18:48:20 +0300

    added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/3t-pwa/",
   plugins: [vue(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
